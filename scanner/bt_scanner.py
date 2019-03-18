b11b8ac8ef495609bf48bbc56ecd69c7f18c5a91
Alex <alex.naga@hotmail.com>
2019-03-18 13:09:49 +0100

    Publish data as MQTT

--- scanner/bt_scanner.py
+++ scanner/bt_scanner.py
@@ -2,40 +2,47 @@ from datetime import datetime
 from time import sleep
 import bluetooth
 import json
-# import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt
 
-physical_area = "room0"
+# Config the location for this device
+physical_area = "lnu/campus/classroom_0"
 
 # Config the MQTT client
-# device_id = "rpi0w_0"
-# mqttc = mqtt.Client(device_id)
-# mqtt_server = "test.mosquitto.org"
-# mqtt_port = 1883
-# mqttc.connect(mqtt_server, mqtt_port)
+device_id = "rpi0w_0"
+mqttc = mqtt.Client(client_id=device_id)
+
+# Connect to the MQTT broker
+mqtt_server = "192.168.10.235"
+mqtt_port = 1883
+mqttc.connect(mqtt_server, mqtt_port)
 
 
 def find_devices():
-    """Scan for nearby Bluetooth devices"""
+    """Scans for nearby Bluetooth devices"""
     return bluetooth.discover_devices(duration=2)
 
 
-def to_json(data):
-    json_data = {}
+def to_json(devices):
+    """Encodes the data to JSON"""
+    data = {}
 
-    # Loop through the MAC addresses
-    for count, mac_address in enumerate(data):
-        json_data[count] = mac_address
+    # Loop through the devices
+    for count, mac_address in enumerate(devices):
+        data[count] = mac_address
 
+    json_data = json.dumps(data)  # Encode to JSON
     return json_data
 
 
 def send_data(data):
-    data_out = json.dumps(data)  # encode to JSON
-    # client.publish("topic/" + physical_area, "Hello world!");
+    """Sends the data to the MQTT broker"""
+    print(data)
+    mqttc.publish(physical_area, data)
 
 
 while True:
     nearby_devices = find_devices()
+    print(nearby_devices)
     is_empty = len(nearby_devices) < 1
 
     time = datetime.now().strftime("%H:%M")
@@ -45,8 +52,5 @@ while True:
     if is_empty:
         continue
 
-    data = to_json(nearby_devices)
-
-    print(data)
-    send_data(data)
-
+    json_data = to_json(nearby_devices)
+    send_data(json_data)

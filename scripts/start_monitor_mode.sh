157ca5c51bda9f26c1e703b386be0047c03f9f96
Alex <alex.naga@hotmail.com>
2019-05-05 21:58:00 +0200

    Update script for monitor mode

--- scripts/start_monitor_mode.sh
+++ scripts/start_monitor_mode.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# To check for interfaces: sudo iwconfig
-# To check for interfaces: sudo ip a
-# To check for interfaces: sudo iw dev
+# To check for interfaces: iwconfig
 
 INTERFACE="wlan0"
 
 # Start monitor mode
 sudo airmon-ng start $INTERFACE
 
-# Restart the interface
-#ifdown $INTERFACE
-#ifup $INTERFACE
+# Restart DHCP service for WiFi interface
+sudo dhclient $INTERFACE
--- scripts/stop_monitor_mode.sh
+++ scripts/stop_monitor_mode.sh
@@ -5,5 +5,5 @@ INTERFACE="wlan0mon"
 # Stop monitor mode
 sudo airmon-ng stop "{$INTERFACE}"
 
-# Restart DHCP service
+# Restart DHCP service for WiFi interface
 sudo dhclient $INTERFACE
